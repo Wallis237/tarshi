c54f9f3561a67dd7c31c78ebe6eec06d86b1dec1
Tarshi Williams <tarshiwilliams476@gmail.com>
2025-05-01 08:20:15 -0700

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -21,7 +21,7 @@ const AboutSection = () => {
             <div className="relative">
               <div className="w-full aspect-square relative z-10">
                 <img 
-                  src={`/lovable-uploads/wallispc.jpg${cacheBuster}`}
+                  src={`public/lovable-uploads/856c7f5f-9e7f-41e1-b11b-f83934a4a06d.png${cacheBuster}`}
                   alt="Tarshi Williams working"
                   className="w-full h-full object-cover rounded-lg shadow-lg"
                 />
